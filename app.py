40e2e56c4983464e88f21ef142215861382e3222
abyanmuhammed321-max <abyanmuhammed321@gmail.com>
2026-08-22 20:17:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from google.genai import types
 # 1. Page Configuration & Adaptive Layout
 st.set_page_config(
     page_title="Metaverse AI",
-    page_icon="https://cdn-icons-png.flaticon.com/512/8266/8266933.png",
+    page_icon="⚡",
     layout="centered",
     initial_sidebar_state="auto"
 )
